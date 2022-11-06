a65e7e15fb63c012b9eb1089320eef9a62c00793
xzb1 <88929959+xzb1@users.noreply.github.com>
2022-11-06 19:58:37 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/sh -e
 
 start() {
-	wget https://github.com/hdbjlizhe/fanli/releases/download/debug/autMan_arm64.tar.gz ;
-	tar -zxvf autMan_arm64.tar.gz ; 
-	rm -rf autMan_arm64.tar.gz ;
-	chmod 777 autMan;
-	pkill -9 autMan;
-        autMan -t;
-  	echo "初始化完成, 启动..."
+	s=autMan;f=fanli;a=arm64;if [[ $(uname -a | grep "x86_64") != "" ]];then a=amd64;fi ;if [ ! -d $s ];then mkdir $s;fi ;cd $s;wget https://github.com/hdbjlizhe/${f}/releases/download/debug/autMan_$a.tar.gz && tar -zxvf autMan_$a.tar.gz && rm -rf autMan_$a.tar.gz && chmod 777 $s;pkill -9 $s;$(pwd)/$s -t;
 }
 
 start
